0301055f25c57b944a5e7a53f09c50ddea464469
Dhakshin Krishna <dozerex007@gmail.com>
2021-10-24 11:35:44 +0530

    added signin php page

--- ShopDoc/resources/signIn/signInto.php
+++ ShopDoc/resources/signIn/signInto.php
@@ -1,16 +1,15 @@
 <?php
 
 $name=$_POST["name"];
-$email=$_POST["email"];
 $pass=$_POST["pass"];
 $conf=$_POST["confpass"];
 $age=$_POST["age"];
 $work=$_POST["work"];
 $gen=$_POST["gen"];
 $bday=$_POST["bday"];
-$address=$_POST["ad1"]+", "+$_POST["ad2"]+", "+$_POST["city"]+", "+$_POST["state"]+", "+$_POST["country"];
+$address=$_POST["ad1"]." -|- ".$_POST["ad2"]." -|- ".$_POST["city"]." -|- ".$_POST["state"]." -|- ".$_POST["cou"];
 
 
-echo "<br><br>".$name."<br><br>".$email."<br><br>".$pass."<br><br>".$age."<br><br>".$work."<br><br>".$gen."<br><br>".$bday."<br><br>".$address;
+echo "<br><br>".$name."<br><br>".$pass."<br><br>".$age."<br><br>".$work."<br><br>".$gen."<br><br>".$bday."<br><br>".$address;
 
 ?>
